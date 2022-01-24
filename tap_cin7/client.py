6fdc0de433bac1afebde3de5928c7d2ad02fac73
Shahzaib Ali <shahzaib81399@gmail.com>
2022-01-24 19:01:07 +0100

    CIN7 Task Finalized

---
+++ tap_cin7/client.py
@@ -0,0 +1,116 @@
+"""REST client handling, including CIN7Stream base class."""
+
+from black import main
+import requests
+from pathlib import Path
+from typing import Any, Dict, Optional, Union, List, Iterable
+import time
+import json
+from memoization import cached
+
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.streams import RESTStream
+from singer_sdk.authenticators import BasicAuthenticator
+
+
+SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+
+
+class CIN7Stream(RESTStream):
+    """CIN7 stream class."""
+
+    url_base = "https://api.cin7.com/api"
+
+    # OR use a dynamic url_base:
+    # @property
+    # def url_base(self) -> str:
+    #     """Return the API URL root, configurable via tap settings."""
+    #     return self.config["api_url"]
+
+    def limit_gen():
+        num = 1
+        while True:
+            yield num
+            num += 1
+    
+    next_page = limit_gen()
+    check_empty_response = True
+
+
+    records_jsonpath = "$[*]"  # Or override `parse_response`.
+    next_page_token_jsonpath = ""  # Or override `get_next_page_token`.
+
+    @property
+    def authenticator(self) -> BasicAuthenticator:
+        """Return a new authenticator object."""
+        return BasicAuthenticator.create_for_stream(
+            self,
+            username=self.config.get("api_key"),
+            password=self.config.get("api_password"),
+        )
+
+    @property
+    def http_headers(self) -> dict:
+        """Return the http headers needed."""
+        headers = {}
+        if "user_agent" in self.config:
+            headers["User-Agent"] = self.config.get("user_agent")
+        # If not using an authenticator, you may also provide inline auth headers:
+        # headers["Private-Token"] = self.config.get("auth_token")
+        return headers
+    
+
+
+    def get_next_page_token(
+        self, response: requests.Response, previous_token: Optional[Any]
+    ) -> Optional[Any]:
+        """Return a token for identifying next page or None if no more pages."""
+        # TODO: If pagination is required, return a token which can be used to get the
+        #       next page. If this is the final page, return "None" to end the
+        #       pagination loop.
+        if previous_token is None:
+
+            previous_token = self.next_page.__next__()
+            next_page_token = previous_token
+        else:
+            if self.check_empty_response:
+                next_page_token = self.next_page.__next__()
+            else:
+                next_page_token = None
+        time.sleep(1)
+
+        return next_page_token
+
+    def get_url_params(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> Dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization."""
+        params: dict = {}
+        if next_page_token:
+            params["page"] = next_page_token
+        if self.replication_key:
+            params["sort"] = "asc"
+            params["order_by"] = self.replication_key
+        return params
+
+    def prepare_request_payload(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> Optional[dict]:
+        """Prepare the data payload for the REST API request.
+
+        By default, no payload will be sent (return None).
+        """
+        # TODO: Delete this method if no payload is required. (Most REST APIs.)
+        return None
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result rows."""
+        # TODO: Parse response body and return a set of records.
+        if len(json.loads(response.text))==0:
+            self.check_empty_response = False
+        yield from extract_jsonpath(self.records_jsonpath, input=response.json())
+
+    def post_process(self, row: dict, context: Optional[dict]) -> dict:
+        """As needed, append or transform raw data to match expected structure."""
+        # TODO: Delete this method if not needed.
+        return row
---
+++ tap_cin7/tap.py
@@ -0,0 +1,59 @@
+"""CIN7 tap class."""
+
+from typing import List
+
+from singer_sdk import Tap, Stream
+from singer_sdk import typing as th  # JSON schema typing helpers
+# TODO: Import your custom stream types here:
+from tap_cin7.streams import (
+    ProductStream,
+    PurchaseOrdersStream,
+    VoucherStream,
+    OrderStream
+
+)
+# TODO: Compile a list of custom stream types here
+#       OR rewrite discover_streams() below with your custom logic.
+STREAM_TYPES = [
+    ProductStream,
+    PurchaseOrdersStream,
+    VoucherStream,
+    OrderStream
+]
+
+
+class TapCIN7(Tap):
+    """CIN7 tap class."""
+    name = "tap-cin7"
+
+    # TODO: Update this section with the actual config values you expect:
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "api_key",
+            th.StringType,
+            required=True,
+            description="api_key"
+        ),
+        th.Property(
+            "api_password",
+            th.StringType,
+            required=True,
+            description="api_password"
+        ),
+        th.Property(
+            "start_date",
+            th.DateTimeType,
+            description="The earliest record date to sync"
+        ),
+    ).to_dict()
+
+    def discover_streams(self) -> List[Stream]:
+        """Return a list of discovered streams."""
+        return [stream_class(tap=self) for stream_class in STREAM_TYPES]
+
+
+def main():
+    TapCIN7.cli()
+
+if __name__ == "__main__":
+    main()

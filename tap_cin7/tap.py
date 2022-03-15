4f5a34c45e70d648e9bd604acdebf47fc3b23dd7
Josué M. Sehnem <josue@sehnem.com>
2022-03-14 21:37:33 -0400

    fix return character

--- tap_cin7/tap.py
+++ tap_cin7/tap.py
@@ -3,8 +3,8 @@
 from typing import List
 
 from singer_sdk import Tap, Stream
-from singer_sdk import typing as th  # JSON schema typing helpers
-# TODO: Import your custom stream types here:
+from singer_sdk import typing as th
+
 from tap_cin7.streams import (
     ProductStream,
     PurchaseOrdersStream,
@@ -13,8 +13,7 @@ from tap_cin7.streams import (
     VoucherStream
 
 )
-# TODO: Compile a list of custom stream types here
-#       OR rewrite discover_streams() below with your custom logic.
+
 STREAM_TYPES = [
     ProductStream,
     PurchaseOrdersStream,
@@ -28,7 +27,6 @@ class TapCIN7(Tap):
     """CIN7 tap class."""
     name = "tap-cin7"
 
-    # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
         th.Property(
             "api_key",
@@ -54,8 +52,5 @@ class TapCIN7(Tap):
         return [stream_class(tap=self) for stream_class in STREAM_TYPES]
 
 
-def main():
-    TapCIN7.cli()
-
 if __name__ == "__main__":
-    main()
+    TapCIN7.cli()
